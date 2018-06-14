83c72f65993c6088d627c8b6c47b4b08c311aeaa
Scott Soffel <ssoffel@hotmail.com>
2018-06-14 17:58:49 -0400

    fix(bug in UserHome)

--- client/components/scap.js
+++ client/components/scap.js
@@ -1,3 +1,13 @@
-var a = { "symbol": "UAA"}
 
-console.log(a.symbol)
+var stocks = { uaa: {
+  name: 'UA',
+  price: 50
+},
+nke: {
+  name: 'nke',
+  price: 45
+}}
+const nke = { name: 'nke', price: 55 }
+ stocks = Object.assign(stocks, {nke})
+
+console.log(stocks)
